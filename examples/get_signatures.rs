7e66426f3632b5dd59b46089bce7b514ba4297ca
Emanuele Bolognesi <ema.bolo94@gmail.com>
2025-03-22 18:04:59 +0800

    get transactions

--- examples/get_signatures.rs
+++ examples/get_signatures.rs
@@ -1,24 +1,28 @@
+use std::str::FromStr;
+
 use solana_trading_util::{
-    signatures::{get_signatures_since_time, GetSignaturesSinceTimeConfig},
+    signatures::{
+        get_signatures_since_time, get_transactions_from_signatures, GetSignaturesSinceTimeConfig,
+        GetTransactionsFromSignaturesConfig,
+    },
     time::{datetime_from_timestamp_sec, datetime_now},
 };
 
 use solana_client::nonblocking::rpc_client::RpcClient;
-use solana_sdk::commitment_config::CommitmentConfig;
+use solana_sdk::{commitment_config::CommitmentConfig, signature::Signature};
 
-#[tokio::main]
-async fn main() {
-    let rpc = RpcClient::new("https://api.mainnet-beta.solana.com".to_string());
+async fn get_signatures(rpc: &RpcClient) -> Vec<Signature> {
     let since = datetime_now() - chrono::Duration::seconds(45);
 
     println!("requesting signatures since {:?}", since);
     let signatures = get_signatures_since_time(
-        &rpc,
+        rpc,
         GetSignaturesSinceTimeConfig {
             target: solana_sdk::pubkey!("JUP6LkbZbjS1jKKwapdHNy74zcZ3tLUZoi5QNyVTaV4"),
             since_timestamp_sec: since.timestamp(),
             ignore_failed: true,
             commitment: CommitmentConfig::finalized(),
+            log_progress: true,
         },
     )
     .await
@@ -30,4 +34,48 @@ async fn main() {
         "oldest signature time: {:?}",
         datetime_from_timestamp_sec(oldest_block_time).unwrap()
     );
+
+    let signatures = signatures[..100]
+        .to_vec()
+        .iter()
+        .map(|sig| Signature::from_str(&sig.signature).unwrap())
+        .collect::<Vec<_>>();
+    signatures
+}
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_target(false)
+        .init();
+
+    let rpc = RpcClient::new("https://api.mainnet-beta.solana.com".to_string());
+
+    let signatures = get_signatures(&rpc).await;
+    let signatures = signatures[..10].to_vec();
+    println!("{:?}", signatures);
+
+    // let signatures = vec![
+    //     "WUcNkH51Nahq8qwRxZBc4eNrHeHDD1qCu17PS8yLKzbbFmUgj2MGCxLACt4qXkyfu9DJUUqtPwYfc6YaGnRefv9",
+    //     "4ffP9jtxTvbEFKACsbUSAgyYo2iTLu6v5EAyKNvRXu3rZGvUpH4jD7V8N8urRTsooEZ7ih56sjBjCTV5XXgXUzPA",
+    //     "2Hnp2iDEZrT7GivyctukGCuLRrMoFjZqxMZZcdgoGc9bp94FaaxJMGSQbovGwHwqapokzRdj69HZmNEHm4Bb5CD9",
+    //     "4RFVtumw8ypbpSEYi7Cbhb8xDSFhR2NGk8DYv7nUtZxjAAaUbwN2iTnZ2cuNVxToUbNsyYFUaRZohQVCkHNC2XL4",
+    // ]
+    // .iter()
+    // .map(|sig| Signature::from_str(sig).unwrap())
+    // .collect();
+
+    let txs = get_transactions_from_signatures(
+        &rpc,
+        GetTransactionsFromSignaturesConfig {
+            batch_size: 2,
+            signatures,
+            commitment: CommitmentConfig::finalized(),
+            log_progress: true,
+        },
+    )
+    .await;
+
+    println!("{:?}", txs);
 }
--- src/signatures.rs
+++ src/signatures.rs
@@ -1,10 +1,12 @@
 use std::str::FromStr;
 
+use anyhow::Result;
 use solana_client::{
     nonblocking::rpc_client::RpcClient, rpc_client::GetConfirmedSignaturesForAddress2Config,
-    rpc_response::RpcConfirmedTransactionStatusWithSignature,
+    rpc_config::RpcTransactionConfig, rpc_response::RpcConfirmedTransactionStatusWithSignature,
 };
 use solana_sdk::{commitment_config::CommitmentConfig, pubkey::Pubkey, signature::Signature};
+use solana_transaction_status::{EncodedConfirmedTransactionWithStatusMeta, UiTransactionEncoding};
 
 use crate::time::datetime_from_timestamp_sec;
 
@@ -13,15 +15,16 @@ pub struct GetSignaturesSinceTimeConfig {
     pub since_timestamp_sec: i64,
     pub ignore_failed: bool,
     pub commitment: CommitmentConfig,
+    pub log_progress: bool,
 }
 
 /// Returns all the signatures for a given address since a timestamp in seconds.
 /// Signatures are returned in descending order, from the newest to the oldest.
 pub async fn get_signatures_since_time(
-    rpc_client: &RpcClient,
+    rpc: &RpcClient,
     config: GetSignaturesSinceTimeConfig,
-) -> anyhow::Result<Vec<RpcConfirmedTransactionStatusWithSignature>> {
-    let mut signatures = rpc_client
+) -> Result<Vec<RpcConfirmedTransactionStatusWithSignature>> {
+    let mut signatures = rpc
         .get_signatures_for_address_with_config(
             &config.target,
             GetConfirmedSignaturesForAddress2Config {
@@ -40,13 +43,15 @@ pub async fn get_signatures_since_time(
     let mut oldest_blocktime = oldest_signature.block_time.unwrap();
 
     while oldest_blocktime > config.since_timestamp_sec {
-        tracing::debug!(
-            "get_signatures before {:?} {}",
-            datetime_from_timestamp_sec(oldest_blocktime),
-            oldest_signature.signature
-        );
+        if config.log_progress {
+            tracing::info!(
+                "get_signatures before {:?} {}",
+                datetime_from_timestamp_sec(oldest_blocktime),
+                oldest_signature.signature
+            );
+        }
 
-        let prev_signatures = rpc_client
+        let prev_signatures = rpc
             .get_signatures_for_address_with_config(
                 &config.target,
                 GetConfirmedSignaturesForAddress2Config {
@@ -69,4 +74,65 @@ pub async fn get_signatures_since_time(
     }
 
     Ok(signatures)
+}
+
+pub struct GetTransactionsFromSignaturesConfig {
+    pub batch_size: usize,
+    pub signatures: Vec<Signature>,
+    pub commitment: CommitmentConfig,
+    pub log_progress: bool,
+}
+
+pub async fn get_transactions_from_signatures(
+    rpc: &RpcClient,
+    config: GetTransactionsFromSignaturesConfig,
+) -> Result<Vec<EncodedConfirmedTransactionWithStatusMeta>> {
+    let n = config.signatures.len();
+    let mut current_idx_min = 0;
+    let mut current_idx_max = std::cmp::min(config.batch_size, n);
+
+    let mut transactions: Vec<EncodedConfirmedTransactionWithStatusMeta> = vec![];
+
+    while current_idx_max <= n {
+        if config.log_progress {
+            tracing::info!(
+                "current_idx_max {}/{} ({:.2}%)",
+                current_idx_max,
+                n,
+                current_idx_max as f64 / n as f64 * 100.0
+            );
+        }
+
+        let signatures_batch = config.signatures[current_idx_min..current_idx_max].to_vec();
+
+        let requests = signatures_batch
+            .iter()
+            .map(|sig| {
+                rpc.get_transaction_with_config(
+                    sig,
+                    RpcTransactionConfig {
+                        encoding: Some(UiTransactionEncoding::JsonParsed),
+                        commitment: Some(CommitmentConfig::confirmed()),
+                        max_supported_transaction_version: Some(0),
+                    },
+                )
+            })
+            .collect::<Vec<_>>();
+
+        for res in futures::future::join_all(requests).await {
+            match res {
+                Ok(tx) => transactions.push(tx),
+                Err(err) => tracing::error!("{}", err),
+            }
+        }
+
+        if current_idx_max == n {
+            break;
+        }
+
+        current_idx_min = current_idx_max;
+        current_idx_max = std::cmp::min(current_idx_max + config.batch_size, n);
+    }
+
+    Ok(transactions)
 }
